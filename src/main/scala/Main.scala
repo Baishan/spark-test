fd626ef54ef4b0b9c14819713ba4f03b413d63ec
Brian Cullen <brian@fractal.home>
2018-05-09 00:21:24 +0100

    simple spark

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,18 @@
+import org.apache.spark.sql.SparkSession
 
-object Main extends App {
-  val l : Int = 43
-  println("Hello, Brian!")
+object Main {
+  def main(args: Array[String]){
+    // val config = new SparkConf
+    // val sc = new SparkContext("local", "Test", config)
+    val spark = SparkSession.builder.appName("test")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+    import spark.implicits._
+    val file = spark.read.textFile("example.json")
+    val lines = file.map(line => line)
+    lines.show()
+
+  }
 }
 
